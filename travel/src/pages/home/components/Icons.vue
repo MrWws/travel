2063dba9d4ba13efdeaf411f85658552ff938355
MrWws <1223645048wws>
2021-06-17 10:58:12 +0800

    add icons

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+      <!-- 用于分页 提升用户体验 -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      iconList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+          desc: '滑雪季'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+          desc: '动植园'
+        },
+        {
+          id: '0005',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0006',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+          desc: '滑雪季'
+        },
+        {
+          id: '0007',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: '0008',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+          desc: '动植园'
+        },
+        {
+          id: '0009',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+          desc: '动植园'
+        }
+      ]
+    }
+  },
+  // 计算属性里的算法就是帮助我们把一维的九条数组拆分成二维数组
+  // 然后页码和对应的数据项做一个关联
+  computed: {
+    pages() {
+      const pages = []
+      // forEach() 方法用于调用数组的每个元素，并将元素传递给回调函数。
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// ~默认代表当前项目的根目录
+//styles为别名
+// alias: {
+//       'vue$': 'vue/dist/vue.esm.js',
+//       '@': resolve('src'),
+//       'styles': resolve('src/assets/styles'),
+//     }
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      height: 0
+      position: relative
+      overflow: hidden
+      width: 25%
+      padding-bottom: 25%
+      float: left
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right 0
+        bottom .44rem
+        .icon-img-content
+          height 100%
+          display block
+          margin 0 auto
+      .icon-desc
+        position absolute
+        left: 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        text-align center
+        color $darkTextColor
+        ellipsis()
+</style>
+
--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -1,44 +1,65 @@
 <template>
-  <div class="wrapper">
-  <swiper :options="swiperOption">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
-      <img class="swiper-img" :src="item.imgUrl" />
-    </swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+  <!-- 在swiper外面加上一层div，是为了防止在网速慢的情况下抖动的bug，用户体验不好 -->
+  <div class="warpper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- 用于分页 -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeSwiper',
-  data() {
+  // ES6 data后面要有空格
+  data () {
     return {
       swiperOption: {
+        // 参数选项,显示小点
         pagination: '.swiper-pagination',
-        loop: true
+        // 循环轮播
+        loop: true,
+        // 每张播放时长1秒，自动播放
+        autoplay: 1000,
+        // 滑动速度
+        speed: 500
       },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1501/31/31826775e41f6b0b.water.jpg_600x330_f457ede5.jpg'
-      },{
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/2003/23/23a8b48c0b65370aa3.img.jpg_600x330_e32e0a31.jpg'
-      }]
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1801/93/ce59d182aca07102.jpg_640x200_ba03d44c.jpg'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.wrapper >>> .swiper-pagination-bullet-active
-  background: #fff
-  .wrapper
-    overflow: hidden
-    width: 100%
-    height: 0
-    padding-bottom: 55%
-    background: #eee
-    .swiper-img
-      width :100%
+// 样式进行了穿透  只要warpper下出现swiper-pagination-bullet-active类名就变色
+// 这样就不受scoped作用域的限制
+.warpper >>> .swiper-pagination-bullet-active {
+  background-color: #fff !important;
+}
+.warpper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
 </style>
+
